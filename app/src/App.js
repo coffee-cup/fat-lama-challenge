71967e162c746b52c18a51617b3458e9c91c83e1
Jake Runzer <jakerunzer@gmail.com>
2018-07-23 01:34:08 -0700

    transaction view shows information about the transaction

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from './components/Header.js';
+import { colours } from './styles.js';
 
 import FeedView from './FeedView.js';
 import TransactionView from './TransactionView.js';
 
 const Wrapper = styled.div`
-  max-width: 60rem;
+  max-width: 80rem;
   margin: auto;
+  padding: 0 2rem;
+  color: ${colours.dark};
 `;
 
 class App extends Component {
@@ -16,9 +19,9 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Header />
-
           <Wrapper>
+            <Header />
+
             <Route exact path="/" component={FeedView} />
             <Route path="/tx/:id" component={TransactionView} />
           </Wrapper>
--- app/src/TransactionView.js
+++ app/src/TransactionView.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { getTransactionById } from './api.js';
+import Title from './components/Title.js';
+
+const Row = styled.div`
+  padding: 0.8rem 0;
+
+  > span:first-child {
+    display: inline-block;
+    padding-right: 1rem;
+    min-width: 7rem;
+    font-weight: bold;
+  }
+`;
+
+const InfoCell = ({ name, value }) => (
+  <Row>
+    <span>{name}</span>
+    <span>{value}</span>
+  </Row>
+);
 
 class TransactionView extends Component {
   constructor(props) {
@@ -19,10 +39,21 @@ class TransactionView extends Component {
   }
 
   render() {
-    const { transactionId } = this.state;
+    const tx = this.state.transaction;
     return (
       <div className="transaction-view">
-        <h1>Transaction View {}</h1>
+        <Title>Transaction View</Title>
+
+        <div className="transaction-info">
+          <InfoCell name="Id" value={tx.id} />
+          <InfoCell name="Item" value={tx.itemId} />
+          <InfoCell name="Status" value={tx.status} />
+          <InfoCell name="Price" value={tx.price} />
+          <InfoCell name="Currency" value={tx.currency} />
+          <InfoCell name="Discount" value={tx.totalDiscount} />
+          <InfoCell name="Borrower" value={tx.borrowerId} />
+          <InfoCell name="Lender" value={tx.lenderId} />
+        </div>
       </div>
     );
   }
